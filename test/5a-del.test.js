d1b3a305f359d2f5a24b312256700bec0d828ba9
Brian J Brennan <brianloveswords@gmail.com>
2013-11-24 11:47:17 -0500

    Test with sqlite

---
+++ test/5a-del.test.js
@@ -0,0 +1,26 @@
+const test = require('tap').test
+const base = require('..')
+const sqliteLoad = require('./sqlite-load')
+
+const db = base.connect({
+  driver: 'sqlite',
+  database: 'whatever.db',
+})
+
+test('table.get', function (t) {
+  sqliteLoad(db, ['user-sqlite'], function () {
+    const user = db.table('user', {
+      fields: ['first_name', 'last_name'],
+    })
+
+    user.del({last_name: 'Hannah'}, function (err, result) {
+      t.notOk(err, 'no errors')
+      t.same(result.affectedRows, 1, 'should affect one row')
+
+      user.get({last_name: 'Hannah'}, function (err, rows) {
+        t.same(rows, [], 'should have no results')
+        t.end()
+      })
+    })
+  })
+})
