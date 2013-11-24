6c614f29c2f5cbe95c232398cff91e9d3d912df4
Brian J Brennan <brianloveswords@gmail.com>
2013-11-24 16:17:02 -0500

    Basic sqlite createReadStrea

---
+++ test/6a-createreadstream.test.js
@@ -0,0 +1,155 @@
+const test = require('tap').test
+const concat = require('concat-stream')
+const sqliteLoad = require('./sqlite-load')
+const base = require('..')
+
+const db = base.connect({
+  driver: 'sqlite',
+  database: 'whatever.db',
+})
+
+const tables = ['user-sqlite', 'book-sqlite', 'profile-sqlite']
+
+test('table.createReadStream: basic', function (t) {
+  sqliteLoad(db, tables, function () {
+    const book = makeBookDb(db)
+    const rs = book.createReadStream()
+    rs.pipe(concat(function (streamRows) {
+      book.get(function (err, getRows) {
+        t.same(getRows, streamRows, 'should get the same rows')
+        t.end()
+      })
+    }))
+    rs.on('error', function (err) {
+      t.fail('should not have an error')
+      throw err
+    })
+  })
+})
+
+// test('table.createReadStream: limits and pages', function (t) {
+//   useDb(t, tables, function (db, done) {
+//     const book = makeBookDb(db)
+
+//     book.createReadStream({}, {
+//       limit: 1,
+//       page: 2,
+//       debug: true,
+//     }).pipe(concat(function (data) {
+//       t.same(data.length, 1)
+//       t.same(data[0].title, 'Pastoralia')
+//       t.end()
+//     }))
+//   })
+// })
+
+// test('table.createReadStream: hasOne relationships', function (t) {
+//   useDb(t, tables, function (db, done) {
+//     const author = makeUserDb(db)
+//     const book = makeBookDb(db)
+//     const profile = makeProfileDb(db)
+
+//     book.createReadStream({}, {
+//       debug: true,
+//       // include: ['bio', 'first_name', 'last_name'],
+//       exclude: ['release_date', 'title'],
+//       relationships: {
+//         author: {
+//           type: 'hasOne',
+//           local: 'author_id',
+//           foreign: { table: 'user', key: 'id' },
+//         },
+//         sameAuthor: {
+//           type: 'hasOne',
+//           local: { table: 'book', key: 'author_id'},
+//           foreign: {
+//             table: 'user', as: 'yasah',
+//             key: 'id',
+//           }
+//         },
+//         profile: {
+//           optional: true,
+//           type: 'hasOne',
+//           local: { table: 'yasah', key: 'id'},
+//           foreign: {
+//             table: 'profile', as: 'whatevar',
+//             key: 'author_id',
+//           },
+//         },
+//       },
+//     }).pipe(concat(function(rows){
+//       t.ok(rows.length > 1, 'should have more than one row')
+//       t.same(rows[0].authorFullName(), 'George Saunders')
+//       t.same(rows[0].sameAuthor.fullName(), 'George Saunders')
+//       t.same(rows[0].profile.bio, 'Used to be a geophysical engineer')
+//       t.end()
+//     }))
+
+//   })
+// })
+
+
+// // test('table.createReadStream: hasMany relationships', function (t) {
+// //   useDb(t, tables, function (db, done) {
+// //     const user = makeUserDb(db)
+// //     const book = makeBookDb(db)
+// //     const story = makeStoryDb(db)
+// //     const review = makeReviewDb(db)
+
+// //     const bookStream = book.createReadStream({}, {
+// //       relationships: true,
+// //     }).pipe(concat(function (rows) {
+// //       const first = rows[0]
+// //       t.same(first.authorFullName(), first.author.fullName(), 'should have right methods')
+// //       t.same(first.author.fullName(), 'George Saunders', 'should have right author')
+// //       t.same(first.stories.length, 7, 'should have seven stories')
+// //       t.same(first.stories[0].reverse(), 'enilceD daB ni dnaLraWliviC', 'should have methods on hasMany')
+// //       t.end()
+// //     }))
+// //   })
+// // })
+
+
+
+// function makeProfileDb(db) {
+//   return db.table('profile', {
+//     fields: [ 'id', 'author_id', 'bio' ],
+//   })
+// }
+// function makeUserDb(db) {
+//   return db.table('user', {
+//     fields: [ 'id', 'first_name', 'last_name' ],
+//     methods: {
+//       fullName: function authorFullName() {
+//         return [this.first_name, this.last_name].join(' ')
+//       }
+//     },
+//   })
+// }
+// function makeStoryDb(db) {
+//   return db.table('story', {
+//     fields: [ 'id', 'book_id', 'title', ],
+//     methods: {
+//       reverse: function reverse() {
+//         return this.title.split('').reverse().join('')
+//       }
+//     }
+//   })
+// }
+// function makeReviewDb(db) {
+//   return db.table('review', {
+//     fields: ['id', 'book_id', 'link']
+//   })
+// }
+
+function makeBookDb(db) {
+  return db.table('book', {
+    fields: [ 'id', 'author_id', 'title', 'release_date' ],
+    methods: {
+      authorFullName: function authorFullName() {
+        const author = this.author
+        return [author.first_name, author.last_name].join(' ')
+      }
+    },
+  })
+}
---
+++ test/sql/book-sqlite.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `author_id` BIGINT NOT NULL,
+  `title` VARCHAR(255) NOT NULL UNIQUE,
+  `release_date` VARCHAR(255) NOT NULL
+);
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (1, 'CivilWarLand in Bad Decline', '1996-01');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (1, 'Pastoralia', '2000-06');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (1,'The Very Persistent Gappers of Frip','2000-08');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (1,'The Brief and Frightening Reign of Phil','2005-09');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (1,'In Persuasion Nation','2006-04');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (1,'Tenth of December','2013-01');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (2,'Airships','1978');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (2,'Captain Maximus','1985');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (2,'Bats out of Hell','1993');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (2,'High Lonesome','1996');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (2,'Long, Last, Happy: New and Selected Stories','2010');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (3,'Stranger Things Happen','2000');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (3,'4 Stories','2000');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (3,'Magic for Beginners','2005');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (3,'La jeune détective et autres histoires étranges','2008');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (3,'Pretty Monsters','2008');
+
+INSERT INTO `book` (`author_id`, `title`, `release_date`)
+       VALUES (3,'Origin Stories','2012');
