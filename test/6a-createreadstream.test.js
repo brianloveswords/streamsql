8e5acae5f883c25cfcbb4cbe0592ec8bf2ba73af
Brian J Brennan <brianloveswords@gmail.com>
2013-11-24 19:02:04 -0500

    Mulitple hasMany relationships working for sqlite

--- test/6a-createreadstream.test.js
+++ test/6a-createreadstream.test.js
@@ -10,81 +10,81 @@ const db = base.connect({
 
 const tables = ['user-sqlite', 'book-sqlite', 'profile-sqlite', 'review-sqlite']
 
-// test('table.createReadStream: basic', function (t) {
-//   sqliteLoad(db, tables, function () {
-//     const book = makeBookDb(db)
-//     const rs = book.createReadStream()
-//     rs.pipe(concat(function (streamRows) {
-//       book.get(function (err, getRows) {
-//         t.same(getRows, streamRows, 'should get the same rows')
-//         t.end()
-//       })
-//     }))
-//     rs.on('error', function (err) {
-//       t.fail('should not have an error')
-//       throw err
-//     })
-//   })
-// })
+test('table.createReadStream: basic', function (t) {
+  sqliteLoad(db, tables, function () {
+    const book = makeBookDb(db)
+    const rs = book.createReadStream()
+    rs.pipe(concat(function (streamRows) {
+      book.get(function (err, getRows) {
+        t.same(getRows, streamRows, 'should get the same rows')
+        t.end()
+      })
+    }))
+    rs.on('error', function (err) {
+      t.fail('should not have an error')
+      throw err
+    })
+  })
+})
 
-// test('table.createReadStream: limits and pages', function (t) {
-//   sqliteLoad(db, tables, function () {
-//     const book = makeBookDb(db)
+test('table.createReadStream: limits and pages', function (t) {
+  sqliteLoad(db, tables, function () {
+    const book = makeBookDb(db)
 
-//     book.createReadStream({}, {
-//       limit: 1,
-//       page: 2,
-//       debug: true,
-//     }).pipe(concat(function (data) {
-//       t.same(data.length, 1)
-//       t.same(data[0].title, 'Pastoralia')
-//       t.end()
-//     }))
-//   })
-// })
+    book.createReadStream({}, {
+      limit: 1,
+      page: 2,
+      debug: true,
+    }).pipe(concat(function (data) {
+      t.same(data.length, 1)
+      t.same(data[0].title, 'Pastoralia')
+      t.end()
+    }))
+  })
+})
 
-// test('table.createReadStream: hasOne relationships', function (t) {
-//   sqliteLoad(db, tables, function () {
-//     const author = makeUserDb(db)
-//     const book = makeBookDb(db)
-//     const profile = makeProfileDb(db)
+test('table.createReadStream: hasOne relationships', function (t) {
+  sqliteLoad(db, tables, function () {
+    const author = makeUserDb(db)
+    const book = makeBookDb(db)
+    const profile = makeProfileDb(db)
 
-//     book.createReadStream({}, {
-//       // include: ['bio', 'first_name', 'last_name'],
-//       relationships: {
-//         author: {
-//           type: 'hasOne',
-//           local: 'author_id',
-//           foreign: { table: 'user', key: 'id' },
-//         },
-//         sameAuthor: {
-//           type: 'hasOne',
-//           local: { table: 'book', key: 'author_id'},
-//           foreign: {
-//             table: 'user', as: 'yasah',
-//             key: 'id',
-//           }
-//         },
-//         profile: {
-//           optional: true,
-//           type: 'hasOne',
-//           local: { table: 'yasah', key: 'id'},
-//           foreign: {
-//             table: 'profile', as: 'whatevar',
-//             key: 'author_id',
-//           },
-//         },
-//       },
-//     }).pipe(concat(function(rows){
-//       t.ok(rows.length > 1, 'should have more than one row')
-//       t.same(rows[0].authorFullName(), 'George Saunders')
-//       t.same(rows[0].sameAuthor.fullName(), 'George Saunders')
-//       t.same(rows[0].profile.bio, 'Used to be a geophysical engineer')
-//       t.end()
-//     }))
+    book.createReadStream({}, {
+      // include: ['bio', 'first_name', 'last_name'],
+      relationships: {
+        author: {
+          type: 'hasOne',
+          local: 'author_id',
+          foreign: { table: 'user', key: 'id' },
+        },
+        sameAuthor: {
+          type: 'hasOne',
+          local: { table: 'book', key: 'author_id'},
+          foreign: {
+            table: 'user', as: 'yasah',
+            key: 'id',
+          }
+        },
+        profile: {
+          optional: true,
+          type: 'hasOne',
+          local: { table: 'yasah', key: 'id'},
+          foreign: {
+            table: 'profile', as: 'whatevar',
+            key: 'author_id',
+          },
+        },
+      },
+    }).pipe(concat(function(rows){
+      t.ok(rows.length > 1, 'should have more than one row')
+      t.same(rows[0].authorFullName(), 'George Saunders')
+      t.same(rows[0].sameAuthor.fullName(), 'George Saunders')
+      t.same(rows[0].profile.bio, 'Used to be a geophysical engineer')
+      t.end()
+    }))
 
-//   })
-// })
+  })
+})
 
 
 test('table.createReadStream: hasMany relationships', function (t) {
@@ -102,16 +102,17 @@ test('table.createReadStream: hasMany relationships', function (t) {
           local: 'id',
           foreign: { table: 'review', key: 'book_id' },
         },
-        // mehReviews: {
-        //   type: 'hasMany',
-        //   local: 'id',
-        //   foreign: { table: 'review', key: 'book_id' },
-        // }
+        mehReviews: {
+          type: 'hasMany',
+          local: 'id',
+          foreign: { table: 'review', key: 'book_id' },
+        }
       }
     }).pipe(concat(function (rows) {
       const first = rows[0]
       t.ok(first.reviews.length > 0, 'should have reviews')
       t.ok(first.reviews[0].linkify(), 'should have methods')
+      t.same(first.reviews, first.mehReviews, 'should have two hasMany')
       t.end()
     }))
   })
